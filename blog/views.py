8582fee704c85dfedd595e8a256adcf6737d4dd6
lit-fatfish <416983839@qq.com>
2020-03-25 20:25:37 +0800

    1.2 测试完成提交

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     #context['blog_count'] = Blog.objects.all().count()  #统计博客数量，然后可以直接在html中引用
     return render_to_response('blog/blog_list.html', context)
 
@@ -15,7 +16,8 @@ def blog_detail(request, blog_pk):
 def blog_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_with_type.html', context)
 

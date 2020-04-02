beb4894da191e90e071284787630f962d6302fd4
lit-fatfish <416983839@qq.com>
2020-04-02 17:28:03 +0800

    1.6 分类统计和归档的数量显示

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response,get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Count
 from .models import Blog, BlogType
 from django.conf import settings
 
@@ -24,12 +25,20 @@ def get_blog_list_common_data(request, blogs_all_list):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
+    # 获取日期归档对应的博客数量
+    blog_dates = Blog.objects.dates('created_time', 'month', order="DESC")
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count = Blog.objects.filter(created_time__year=blog_date.year,
+                                        created_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date] = blog_count
+
     context = {}
     context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
-    context['blog_types'] = BlogType.objects.all()
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog')) #scan语句   
     context['page_range'] = page_range
-    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order="DESC")
+    context['blog_dates'] = blog_dates_dict
 
     return context
 
@@ -44,8 +53,9 @@ def blog_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
 
-    context = get_blog_list_common_data(request, blogs_all_list)
     context['blog_types'] = BlogType.objects.all()
+    context = get_blog_list_common_data(request, blogs_all_list)
+    
 
     return render_to_response('blog/blog_with_type.html', context)
 

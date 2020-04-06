3c72b3e9c02ea4883ff0b7516f2d1884e84b8f59
lit-fatfish <416983839@qq.com>
2020-04-06 15:00:02 +0800

    2.0 访问计数和显示

--- read_statistics/admin.py
+++ read_statistics/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import ReadNum
+from .models import ReadNum, ReadDetail
 
 # Register your models here.
 
 @admin.register(ReadNum)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('read_num', 'content_object')
+
+@admin.register(ReadDetail)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('date', 'read_num', 'content_object')

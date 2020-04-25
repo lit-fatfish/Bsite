2a4a36149caff46a67542d5ccf4cc2347bbbe630
lit-fatfish <416983839@qq.com>
2020-04-25 21:47:29 +0800

    3.4 修改为mysql数据库

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,11 +83,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'css',
+        'PASSWORD': 'css12345',
+        'HOST': '', #空代表当前主机
+        'PORT': '3306',
     }
 }
 

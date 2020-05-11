6848b7efb1f60cc8a45b5bb28ff1b271b74a9b03
lit-fatfish <416983839@qq.com>
2020-05-11 21:59:29 +0800

    修复footer不能一直显示在底部，注册登录界面在手机端的显示比例

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('guestbook', views.guestbook, name='guestbook'),
+    path('', views.guestbook, name='guestbook'),
     path('message_commit', views.message_commit, name='message_commit'),
 
     
